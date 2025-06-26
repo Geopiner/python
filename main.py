6a3070812a49cda871f80ddbf3509e2b4c78bf6e
George Piner <44291863-maingp93@users.noreply.replit.com>
2025-06-26 14:37:21 +0000

    adding python learning repo

---
+++ main.py
@@ -0,0 +1,77 @@
+# # Comments
+
+# # Variables
+# # print('Hello World')
+# # print('George')
+
+# # name = 'George'
+# # age = 32
+
+# # print(name)
+# # print(age)
+
+# # full_name = 'George Piner'
+# # print(full_name)
+
+# # width, height = 400, 500
+# # print(width)
+# # print(height)
+
+# # # your_name = input('Please enter your name: ')
+# # # print('Hi '+ your_name)
+
+# # num1 = input('Enter a number: ')
+# # num2 = input('Enter another number: ')
+# # print(int(num1) + int(num2))
+
+# # DATA TYPES
+# # strings
+# # int (1, 2, 3), float/decimal (0.2) (numbers)
+
+# # strings 'hello', "cookie" => "hellocookie"
+# # strings '10', "20" => "1020"
+
+# # math operators +, -, /, //(whole number), *, **, %
+# # Tip Calculator App
+# food_amount = float(input('Enter food amount $: '))
+# tip_percentage = float(input('Enter your tip percentage %: ')) / 100
+# tip_amount = food_amount * tip_percentage
+
+# # Total 👉 food_amount + tip_amount
+# total = food_amount + tip_amount
+# print('-------------------------------')
+# print(f'Food Amount: ${food_amount}')
+# print(f'Tip Amount: ${tip_amount}')
+# print('\n')
+# print('Total Amount: $' + str(total))
+# print('-------------------------------')
+
+
+# # string formatting
+
+# # BOOLEAN
+# # IF THEN ELSE
+# # if withdrawal amount > balance:
+# #    don't allow withdraw
+# # else:
+# #     allow withdrawal
+
+# # baby weather app
+# # if it's raining outside, grab an umbrella
+# # otherwise, grab your sunglasses
+
+weather = input('How is the weather? ')
+
+if weather == 'rain':
+  print('☔')
+
+if weather == 'cloudy':
+  print('☁️')
+
+if weather == 'thunderstorm':
+  print('⛈️')
+
+else:
+  print('😎')
+#  comparison operators (==, <, >, <=, >=)
+
